68189142163079139d9c039031834b7f9e50148d
Shakil khan <89624867+Shakilkhan24@users.noreply.github.com>
2025-03-11 20:43:27 +0600

    Update client.py

--- ABC/client.py
+++ ABC/client.py
@@ -4,9 +4,4 @@ from google import genai
 import time
 
 # Initialize the Gemini client
-client = genai.Client(api_key="AIzaSyAcD1SFmVkmfefm7PXUw2WKVaEAqAMhHOs")
-
-# AIzaSyAcD1SFmVkmfefm7PXUw2WKVaEAqAMhHOs
-
-
-# AIzaSyA4JBfVDzO8BJUcMZ3WneGdkv59V3GVQX4
+client = genai.Client(api_key="")
